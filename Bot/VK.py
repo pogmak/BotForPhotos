970d2e160418b141cdf06212bfa42b1ef78c054d
makar <pomakaron@yandex.ru>
2020-04-11 19:01:14 +0300

    Change database logic

--- Bot/VK.py
+++ Bot/VK.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 import random
 import math
 import psycopg2
+import logging
+import os
+
 
 dbname = 'bot'
 dbuser = 'bot'
@@ -12,6 +15,22 @@ vk_session = vk_api.VkApi('+79991399680', 'makpog9814')
 vk_session.auth()
 
 vk = vk_session.get_api()
+
+errorlog = os.path.join('LOG.txt')
+
+#LOGGER
+logger = logging.getLogger('VK')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+eh = logging.FileHandler(errorlog)
+ch.setLevel(logging.DEBUG)
+eh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s')
+eh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(eh)
+logger.addHandler(ch)
 
 
 def loadfromdb():
@@ -41,13 +60,29 @@ def createIntoDB():
         photos.append(vk.photos.get(album_id='saved', offset=j * 1000, rev=0, count=1000))
     with psycopg2.connect(dbname=dbname, user=dbuser, password=dbpassword, host="127.0.0.1", port="5432") as conn:
         with conn.cursor() as cursor:
-            #Delete table and restart sequence
-            cursor.execute("DELETE FROM likes;")
-            cursor.execute("ALTER sequence likes_id_seq restart;")
             for saved_photos in photos:
                 for items in saved_photos['items']:
-                    query_create = "INSERT INTO likes (photo_id,url,likes_count) VALUES ({},'{}',0);".format(items['id'],items['sizes'][-1]['url'])
-                    cursor.execute(query_create)
+                    id = items['id']
+                    url = items['sizes'][-1]['url']
+                    #First, check VK id in database, if not exist - add
+                    query_find = "SELECT * FROM likes WHERE id={}".format(id)
+                    cursor.execute(query_find)
+                    if not cursor.fetchall():
+                        query_create = "INSERT INTO likes (photo_id,url,likes_count) VALUES ({},'{}',0);".format(id,url)
+                        cursor.execute(query_create)
+                        logger.info("Photo with id={} is not in database, insert...".format(id))
+                query_count = "SELECT COUNT(*) FROM likes;"
+                cursor.execute(query_count)
+                count = cursor.fetchall()[0][0]
+                for i in range(0,int(count)):
+                    query_find = "SELECT photo_id FROM likes WHERE id={}".format(count)
+                    cursor.execute(query_find)
+                    ids = [id for id in items['id']]
+                    id = cursor.fetchall()[0[0]]
+                    if not id in ids:
+                        query_delete = "DELETE FROM likes WHERE id={}".format(id)
+                        cursor.execute(query_delete)
+                        logger.info("Photo with id={} in database, but not in VK".format(id))
 
 
 def getByIndex(index):
