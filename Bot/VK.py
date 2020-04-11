99451b6031961a829715f3cfdc685c360bf99128
makar <pomakaron@yandex.ru>
2020-04-11 22:56:23 +0300

    Fix logger to sent message

--- Bot/VK.py
+++ Bot/VK.py
@@ -80,8 +80,9 @@ def SyncDB():
             for i in range(0,count):
                 query_find = "SELECT photo_id FROM likes WHERE id={};".format(i)
                 cursor.execute(query_find)
-                if cursor.fetchall():
-                    id = cursor.fetchall()[0][0]
+                id = cursor.fetchall()
+                if id:
+                    id = id[0][0]
                     if not id in ids:
                         query_delete = "DELETE FROM likes WHERE photo_id={};".format(id)
                         cursor.execute(query_delete)
